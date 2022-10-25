f3631eb9d7a30dab01e341f8a327154fe5ef0912
Simran <ssingh28@ncsu.edu>
2022-10-25 19:17:28 -0400

    Established communication between an app script and inter-connectivity script

---
+++ RawUDP/Spoke/app.py
@@ -0,0 +1,25 @@
+import socket
+import threading
+from time import sleep
+
+DATA_PROC_IP = "0.0.0.0"
+DATA_PROC_PORT =49148
+Message = "Hello, Server"
+
+def receive(sock, ip, port):
+    while True:
+        data = sock.recv(1024)
+        print("Received from Hub: " + data.decode("utf-8"))
+
+def publish(ip, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.connect((ip, port))
+    while True:
+        sleep(2.0)
+        print("sending data to data process.")
+        sock.sendto(bytes(Message, "utf-8"), (ip, port))
+
+threading.Thread(target=publish, args=(DATA_PROC_IP, DATA_PROC_PORT)).start()
+
+
+
--- RawUDP/Spoke/dataProcess.py
+++ RawUDP/Spoke/dataProcess.py
@@ -2,8 +2,10 @@ import socket
 import threading
 from time import sleep
 
-HUB_IP = "18.207.212.184"
+HUB_IP = "44.211.205.92"
 HUB_DATA_PORT = 49152
+APP_PORT =49148
+LOCAL_IP = "0.0.0.0"
 Message = "Hello, Server"
 
 def receive(sock, ip, port):
@@ -11,16 +13,17 @@ def receive(sock, ip, port):
         data = sock.recv(1024)
         print("Received from Hub: " + data.decode("utf-8"))
 
-def publish(sock, ip, port):
+def publish(LOCAL_IP, APP_PORT):
+    appSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    appSock.bind((LOCAL_IP, APP_PORT))
     while True:
-        sleep(2)
-        sock.sendto(bytes(Message, "utf-8"), (ip, port))
+        data = appSock.recv(1024)
+        print("Received from App: " + data.decode("utf-8"))
 
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.connect((HUB_IP, HUB_DATA_PORT))
-sock.sendto(bytes(Message, "utf-8"), (HUB_IP, HUB_DATA_PORT))
-threading.Thread(target=publish, args=(sock,HUB_IP,HUB_DATA_PORT)).start()
-threading.Thread(target=receive, args=(sock, HUB_IP, HUB_DATA_PORT)).start()
+hubSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+hubSock.connect((HUB_IP, HUB_DATA_PORT))
+hubSock.sendto(bytes(Message, "utf-8"), (HUB_IP, HUB_DATA_PORT))
+threading.Thread(target=publish, args=(LOCAL_IP,APP_PORT)).start()
+threading.Thread(target=receive, args=(hubSock, HUB_IP, HUB_DATA_PORT)).start()
 
 
