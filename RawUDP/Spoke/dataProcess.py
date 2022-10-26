85271caa1b87341a5f5ae1e45b21d425de833c77
Simran <ssingh28@ncsu.edu>
2022-10-26 14:35:58 -0400

    Used a constant to store socket buffer size

--- RawUDP/Spoke/dataProcess.py
+++ RawUDP/Spoke/dataProcess.py
@@ -7,23 +7,25 @@ HUB_DATA_PORT = 49152
 APP_PORT =49148
 LOCAL_IP = "0.0.0.0"
 Message = "Hello, Server"
+BUFFER_SIZE = 1024
 
 def receive(sock, ip, port):
     while True:
-        data = sock.recv(1024)
+        data = sock.recv(BUFFER_SIZE)
         print("Received from Hub: " + data.decode("utf-8"))
 
-def publish(LOCAL_IP, APP_PORT):
+def publish(localIP, appPort, hubSock, hubIP, hubPort):
     appSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    appSock.bind((LOCAL_IP, APP_PORT))
+    appSock.bind((localIP, appPort))
     while True:
-        data = appSock.recv(1024)
+        data = appSock.recv(BUFFER_SIZE)
         print("Received from App: " + data.decode("utf-8"))
+        hubSock.sendto(data, (hubIP, hubPort))
 
 hubSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 hubSock.connect((HUB_IP, HUB_DATA_PORT))
 hubSock.sendto(bytes(Message, "utf-8"), (HUB_IP, HUB_DATA_PORT))
-threading.Thread(target=publish, args=(LOCAL_IP,APP_PORT)).start()
+threading.Thread(target=publish, args=(LOCAL_IP, APP_PORT, hubSock, HUB_IP, HUB_DATA_PORT)).start()
 threading.Thread(target=receive, args=(hubSock, HUB_IP, HUB_DATA_PORT)).start()
 
 
